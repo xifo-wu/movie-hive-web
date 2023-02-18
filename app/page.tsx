501e508da2cae33227744df8505bd7e3bf8adb31
Xifo <xifo.wu@gmail.com>
2023-02-18 10:54:13 +0800

    style: 修复首页样式没有撑满错误

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ import LatestSharing from "@/components/LatestSharing";
 
 export default async function Home() {
   return (
-    <main className="relative">
+    <main className="relative flex flex-col min-h-screen">
       <HomeHeader />
       {/* NextJS 正在与 TS 合作解决 TypeScript */}
       {/* @ts-expect-error Server Component */}
       <HomeBannerSection />
-      <div className="container max-w-7xl mx-auto px-4 flex flex-col justify-center">
+      <div className="container max-w-7xl mx-auto px-4 flex flex-col flex-1">
         {/* @ts-expect-error Server Component */}
         <LatestSharing />
       </div>
