6e39bbb7b5d82c7ab1332b5bfb6bc9c2a76afe7b
Xifo <xifo.wu@gmail.com>
2023-02-20 23:38:19 +0800

    feat: 全新 LOGO

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -5,8 +5,8 @@ const HomeHeader = () => {
   return (
     <div className="px-3 w-full absolute left-1/2 -translate-x-1/2 top-3 z-10 max-w-7xl mx-auto flex gap-2 items-center">
       <Link href="/" className="flex gap-2 items-center">
-        <Image src="/logo.png" width={48} height={40} alt="影巢 Logo" />
-        <h1 className="mt-2 text-white" title="影巢 HD Hive">
+        <Image src="/logo.png" width={32} height={32} alt="影巢 Logo" />
+        <h1 className="mt-1 text-white" title="影巢 HD Hive">
           影巢 - HD Hive
         </h1>
       </Link>
