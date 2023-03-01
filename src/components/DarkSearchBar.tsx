f2a864e85a022d11b08f98c8bef3f1b711772303
Xifo <xifo.wu@gmail.com>
2023-03-01 21:29:55 +0800

    feat: 支持回车搜索

--- src/components/DarkSearchBar.tsx
+++ src/components/DarkSearchBar.tsx
@@ -1,37 +1,49 @@
-"use client";
+'use client';
 
 import { useRouter } from 'next/navigation';
-import { ChangeEvent, useState } from "react";
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { TbBrandTelegram } from 'react-icons/tb';
 
 const DarkSearchBar = () => {
+  const { register, handleSubmit } = useForm();
+  const [loading, setLoading] = useState(false);
+
   const router = useRouter();
-  const [query, setQuery] = useState("");
 
-  const handleSearch = () => {
+  const onSubmit = async (values: any) => {
+    const { query } = values;
     if (!query) return;
-
+    setLoading(true);
     router.push(`/share/search?query=${query}`);
-  };
-
-  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setQuery(e.target.value);
+    setLoading(false);
   };
 
   return (
     <div className="relative w-full">
-      <input
-        className="w-full text-slate-600 dark:text-slate-50 text-lg leading-[48px] h-[48px] border-none py-2 px-4 rounded-full focus:outline-none"
-        autoComplete="off"
-        placeholder="输入关键字搜索电影，剧集"
-        onChange={handleSearchChange}
-      />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          className="w-full text-slate-600 dark:text-slate-50 text-lg leading-[48px] h-[48px] border-none py-2 px-4 rounded-full focus:outline-none"
+          autoComplete="off"
+          placeholder="输入关键字搜索电影，剧集"
+          {...register('query')}
+        />
 
-      <button
-        className="absolute right-0 bg-amber-500 h-[48px] text-lg py-2 px-7 hover:bg-amber-600 rounded-full tracking-widest"
-        onClick={handleSearch}
-      >
-        搜索
-      </button>
+        <button
+          type="submit"
+          className="absolute right-0 bg-amber-500 h-[48px] text-lg py-2 px-7 hover:bg-amber-600 rounded-full tracking-widest disabled:opacity-75 disabled:hover:cursor-not-allowed disabled:hover:bg-amber-500"
+          disabled={loading}
+        >
+          {loading ? (
+            <div className="flex gap-2 items-center justify-center animate-[flash_3s_ease-in-out_infinite]">
+              <TbBrandTelegram />
+              搜索中
+            </div>
+          ) : (
+            '搜索'
+          )}
+        </button>
+      </form>
     </div>
   );
 };
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,48 +1,57 @@
-"use client";
+'use client';
 
-import { useRouter } from "next/navigation";
-import { ChangeEvent, useEffect, useState } from "react";
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import { useSearchParams } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import { TbBrandTelegram } from 'react-icons/tb';
 
 const SearchBar = () => {
   const searchParams = useSearchParams();
-
-  const router = useRouter();
-  const [query, setQuery] = useState('');
-
-  const handleSearch = () => {
-    if (!query) return;
-
-    router.push(`/share/search?query=${query}`);
-  };
-
   const urlQuery = searchParams?.get('query');
-  useEffect(() => {
-    if (urlQuery) {
-      setQuery(urlQuery);
-    }
-  }, [urlQuery]);
 
-  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setQuery(e.target.value);
+  const {
+    register,
+    handleSubmit,
+  } = useForm({
+    defaultValues: {
+      query: urlQuery
+    }});
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const onSubmit = async (values: any) => {
+    const { query } = values;
+    if (!query) return;
+    setLoading(true);
+    router.push(`/share/search?query=${query}`);
+    setLoading(false);
   };
-
   return (
     <div className="border border-amber-500 relative w-full rounded-full">
-      <input
-        value={query}
-        className="w-full text-lg leading-[48px] h-[48px] border-none py-2 px-4 rounded-full focus:outline-none"
-        autoComplete="off"
-        placeholder="输入关键字搜索电影，剧集"
-        onChange={handleSearchChange}
-      />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          className="w-full text-slate-600 dark:text-slate-50 text-lg leading-[48px] h-[48px] border-none py-2 px-4 rounded-full focus:outline-none"
+          autoComplete="off"
+          placeholder="输入关键字搜索电影，剧集"
+          {...register('query')}
+        />
 
-      <button
-        className="text-white absolute right-0 bg-amber-500 h-[48px] text-lg py-2 px-7 hover:bg-amber-600 rounded-full tracking-widest"
-        onClick={handleSearch}
-      >
-        搜索
-      </button>
+        <button
+          type="submit"
+          className="absolute right-0 bg-amber-500 h-[48px] text-lg py-2 px-7 hover:bg-amber-600 rounded-full tracking-widest disabled:opacity-75 disabled:hover:cursor-not-allowed disabled:hover:bg-amber-500"
+          disabled={loading}
+        >
+          {loading ? (
+            <div className="flex gap-2 items-center justify-center animate-[flash_3s_ease-in-out_infinite]">
+              <TbBrandTelegram />
+              搜索中
+            </div>
+          ) : (
+            '搜索'
+          )}
+        </button>
+      </form>
     </div>
   );
 };
