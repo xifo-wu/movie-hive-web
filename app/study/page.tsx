646beb2869d8d16fd77c9afdbe4c6b271ca384bf
Xifo <xifo.wu@gmail.com>
2023-03-04 16:32:08 +0800

    feat: 学习页面添加 Header

--- app/study/page.tsx
+++ app/study/page.tsx
@@ -1,21 +1,23 @@
+import Header from '@/components/Header';
 import React from 'react';
 
 const StudyPage = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen h-hull">
-      <div
-        className="w-[500px] h-[500px] rounded-full fixed opacity-25 blur-[100px] top-64 -left-64 right-auto bottom-auto z-[-1]"
-        style={{ backgroundImage: 'linear-gradient(45deg, #f59e0b, #f97316)' }}
-      />
-      <div
-        className="w-[500px] h-[500px] rounded-full fixed opacity-25 blur-[100px] -top-64 left-auto -right-64 bottom-auto z-[-1]"
-        style={{ backgroundImage: 'linear-gradient(45deg, #64748b, #94a3b8)' }}
-      />
+    <>
+      <Header />
+      <div className="flex items-center justify-center min-h-screen h-hull">
+        <div
+          className="w-[500px] h-[500px] rounded-full fixed opacity-25 blur-[100px] top-64 -left-64 right-auto bottom-auto z-[-1]"
+          style={{ backgroundImage: 'linear-gradient(45deg, #f59e0b, #f97316)' }}
+        />
+        <div
+          className="w-[500px] h-[500px] rounded-full fixed opacity-25 blur-[100px] -top-64 left-auto -right-64 bottom-auto z-[-1]"
+          style={{ backgroundImage: 'linear-gradient(45deg, #64748b, #94a3b8)' }}
+        />
 
-      <div className="text-xl">
-        学个屁，不准学～
+        <div className="text-xl">学个屁，不准学～</div>
       </div>
-    </div>
+    </>
   );
 };
 
