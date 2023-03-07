24e602f3b2c43de659f7a299f45d12177f7ae4b3
Xifo <xifo.wu@gmail.com>
2023-03-07 14:56:29 +0800

    feat: 优化首页列表样式

--- src/components/LatestSharing.tsx
+++ src/components/LatestSharing.tsx
@@ -1,6 +1,5 @@
-import { fetchShare } from "@/services/share";
-import Link from "next/link";
-import SharingSimpleCard from "./SharingSimpleCard";
+import { fetchShare } from '@/services/share';
+import ShareCardStyle1 from './ShareCard/Style1';
 
 const LatestSharing = async () => {
   const response = await fetchShare({ per_page: 100 });
@@ -15,15 +14,17 @@ const LatestSharing = async () => {
     <div className="mt-6">
       <div className="w-full flex justify-between items-center  mb-8">
         <h2 className="text-2xl font-medium">
-          最新分享 <span className="text-xl text-slate-400 dark:text-slate-200 block sm:inline"><span className="hidden sm:inline">-</span> Latest Sharing</span>
+          最新分享
+          <span className="text-xl text-slate-400 dark:text-slate-200 block sm:inline">
+            <span className="hidden sm:inline">-</span> Latest Sharing
+          </span>
         </h2>
-        {/* <Link href="/share" className="text-amber-500">更多</Link> */}
       </div>
 
       <div className="mb-5">
-        <div className="flex justify-center md:justify-start gap-6 flex-wrap">
+        <div className="grid grid-cols-2 gap-3 sm:grid-cols-4 md:grid-cols-6">
           {data.map((item: any) => (
-            <SharingSimpleCard data={item} key={item.slug} />
+            <ShareCardStyle1 key={item.slug} data={item} />
           ))}
         </div>
       </div>
